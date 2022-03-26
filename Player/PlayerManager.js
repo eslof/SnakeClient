d3269fc9b5f76ebbc426412d8576776ab87190ac
eslof <eslof.github@gmail.com>
2022-03-26 18:58:15 +0100

    some cleanup and changes in dependency

--- Player/PlayerManager.js
+++ Player/PlayerManager.js
@@ -1,54 +1,32 @@
 class PlayerManager {
-    maxPlayers;
-    players;
-    playerTable;
-    onPlayerJoin = null;
-    onPlayerLeave = null;
+    players = [];
 
-    constructor(playerTable, maxPlayers) {
+    constructor(gameData, joinForm, maxPlayers, playerTable) {
+        this.gameData = gameData;
         this.maxPlayers = maxPlayers;
         this.playerTable = playerTable;
+        this.joinForm = joinForm;
     }
 
-    update(playerData) {
-        this.updatePlayers(playerData);
-        this.updatePlayerTable();
+    update() {
+        const playerData = this.gameData.players;
+        this._updatePlayers(playerData);
+        this.playerTable.update(this.players);
+        this.joinForm.update(this.players.length >= this.maxPlayers);
     }
 
-    updatePlayers(playerData) {
+    _updatePlayers() {
+        const playerData = this.gameData.players;
         const players = this.players;
-        let joinCallback = this.onPlayerJoin || function () {};
-        let leaveCallback = this.onPlayerLeave || function () {};
-        if (!(joinCallback instanceof Function)) throw new InternalMisuseError("onPlayerJoin set to other than function or null.");
-        if (!(leaveCallback instanceof Function)) throw new InternalMisuseError("onPlayerLeave set to other than function or null.");
         for (let fd in playerData) {
             if (fd in players) players[fd].update(playerData[fd]);
             else {
                 players[fd] = new Player(playerData[fd]);
-                joinCallback();
             }
             if (players[fd].health === 0) {
                 players[fd].onDelete();
-                leaveCallback();
                 delete players[fd];
             }
-        }
-    }
-
-    updatePlayerTable() {
-        const players = this.players;
-        let i = 0;
-        for (let fd in players) {
-            let rowCells = this.playerTable.rows[i].cells;
-            rowCells[0].innerHTML = players[fd].name;
-            rowCells[1].innerHTML = players[fd].score;
-            i++;
-        }
-
-        for (i < this.maxPlayers; i++;) {
-            let rowCells = this.playerTable.rows[i].cells;
-            rowCells[0].innerHTML = "";
-            rowCells[1].innerHTML = "";
         }
     }
 }
