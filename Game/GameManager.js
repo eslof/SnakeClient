2b9912e928fcf50ed61fa30d9d60e79bb85a3c8d
eslof <eslof.github@gmail.com>
2022-03-26 18:59:00 +0100

    moved away join responsibility

--- Game/GameManager.js
+++ Game/GameManager.js
@@ -1,41 +1,18 @@
 class GameManager {
 
-    constructor(boardManager, canvasManager, gameData, playerManager, joinButton, joinInput, websocket) {
+    constructor(boardManager, canvasManager, gameData, playerManager) {
         this.boardManager = boardManager;
         this.canvasManager = canvasManager;
         this.gameData = gameData;
         this.playerManager = playerManager;
-        this.joinButton = joinButton;
-        this.joinInput = joinInput;
-        this.websocket = websocket;
-        // todo: join stuff shouldnt be here probably
-        joinButton.onclick = () => this.requestJoin();
-        playerManager.onPlayerJoin = () => this.updateJoinUI();
-        playerManager.onPlayerLeave = () => this.updateJoinUI();
-    }
-
-    requestJoin() {
-        this.websocket.send(Request.getJoinRequest(this.joinInput.value));
-        // TODO: handle on join success/fail
-    }
-
-    updateJoinUI() {
-        const playerManager = this.playerManager;
-        const joinButton = this.joinButton;
-        const joinInput = this.joinInput;
-        if (playerManager.players.length >= playerManager.maxPlayers) {
-            joinButton.style.display = joinInput.style.display = "none";
-        } else {
-            joinButton.style.display = joinInput.style.display = "inline-block";
-        }
     }
 
     onMessage(e) {
         const gameData = this.gameData;
         const playerManager = this.playerManager;
         gameData.update(e.data);
-        if (gameData.players.length > 0) playerManager.update(gameData.players);
-        if (gameData.board.length > 0) this.boardManager.update(gameData.board);
+        if (gameData.players.length > 0) playerManager.update();
+        if (gameData.board.length > 0) this.boardManager.draw();
     };
 
     onResize(e) {
