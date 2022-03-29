a2b09eb1bfc7bf8bd3de367906317f164aa8ff0a
eslof <eslof.github@gmail.com>
2022-03-29 02:40:08 +0200

    spacing after type safety

--- Game/GameManager.js
+++ Game/GameManager.js
@@ -9,6 +9,7 @@ class GameManager {
         if (!(canvasManager instanceof CanvasManager)) throw new InternalMisuseError("Wrong parameter type for canvasManager.");
         if (!(gameData instanceof GameData)) throw new InternalMisuseError("Wrong parameter type for gameData.");
         if (!(playerManager instanceof PlayerManager)) throw new InternalMisuseError("Wrong parameter type for playerManager.");
+
         this.boardManager = boardManager;
         this.canvasManager = canvasManager;
         this.gameData = gameData;
