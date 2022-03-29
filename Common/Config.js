18547593248624133cd49dce6c297b6ac6a170a9
eslof <eslof.github@gmail.com>
2022-03-29 12:27:29 +0200

    clean up static unused

--- Common/Config.js
+++ Common/Config.js
@@ -1,8 +1,8 @@
 class Config {
     static MAX_CONCURRENT = 5; // TODO: this should probably come from the server
     static GRID_SIZE = 30; // TODO: this should probably come from the server
-    static IP = "127.0.0.1";
-    static PORT = "80";
+    //static IP = "127.0.0.1";
+    //static PORT = "80";
 
     // TODO: figure out how to handle this
 }
